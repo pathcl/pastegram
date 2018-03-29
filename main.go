edfdd83be345a996a4592a15e5f5ec6a95e4f23b
Luis San Martin <luis@sanmartin.io>
2018-03-29 10:30:13 -0300

    First commit! yey

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+
+	"gopkg.in/telegram-bot-api.v4"
+)
+
+func main() {
+
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("APITELEGRAM"))
+	bot.Debug = false
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	log.Println(bot.Self.UserName)
+
+	consolescanner := bufio.NewScanner(os.Stdin)
+	if err := consolescanner.Err(); err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	for consolescanner.Scan() {
+		input := consolescanner.Text()
+		fmt.Println("Sending to telegram: ", input)
+		msg := tgbotapi.NewMessage(10084376, input)
+		bot.Send(msg)
+	}
+
+}
