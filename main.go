368f2f3ce65fb1483e2559fb88a3d6a62bb671ed
Luis San Martin <luis@sanmartin.io>
2018-04-04 15:56:22 -0300

    Args as input now supported

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"gopkg.in/telegram-bot-api.v4"
 )
@@ -14,23 +15,32 @@ func main() {
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("APITELEGRAM"))
 	bot.Debug = false
 
+	// replace 1111111 with your chatid
+	chatid := int64(1111111)
+
 	if err != nil {
 		log.Panic(err)
 	}
 
-	log.Println(bot.Self.UserName)
+	if len(os.Args) > 1 {
 
-	consolescanner := bufio.NewScanner(os.Stdin)
-	if err := consolescanner.Err(); err != nil {
-		log.Println(err)
-		os.Exit(1)
-	}
-
-	for consolescanner.Scan() {
-		input := consolescanner.Text()
+		log.Println(bot.Self.UserName)
+		input := strings.Join(os.Args[1:], " ")
+		msg := tgbotapi.NewMessage(chatid, input)
 		fmt.Println("Sending to telegram: ", input)
-		msg := tgbotapi.NewMessage(CHATID, input)
 		bot.Send(msg)
-	}
+	} else {
 
+		consolescanner := bufio.NewScanner(os.Stdin)
+		if err := consolescanner.Err(); err != nil {
+			log.Println(err)
+			os.Exit(1)
+		}
+		for consolescanner.Scan() {
+			input := consolescanner.Text()
+			fmt.Println("Sending to telegram: ", input)
+			msg := tgbotapi.NewMessage(chatid, input)
+			bot.Send(msg)
+		}
+	}
 }
